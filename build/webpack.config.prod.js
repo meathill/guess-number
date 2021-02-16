b1a19402443f2cc4d098adea108cd3c63974190a
meathill <meathill@gmail.com>
2021-02-17 00:22:05 +0800

    change: added manifest.webapp

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -1,5 +1,21 @@
 const config = require('./webpack.config');
+const CopyPlugin = require('copy-webpack-plugin');
 
 config.devtool = false;
+config.plugins = [
+  ...config.plugins,
+  new CopyPlugin({
+    patterns: [
+      {
+        from: 'public/manifest.webapp'
+      },
+      {
+        from: 'public/assets',
+        to: 'assets',
+        toType: 'dir',
+      },
+    ]
+  })
+]
 
 module.exports = config;
